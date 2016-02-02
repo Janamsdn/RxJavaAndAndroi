e14682c07c93d0b373a2d6daf442b42ee15b3e8f
Janamsdn <janamsdn@gmail.com>
2016-02-02 21:44:44 +0000

    RxJavaAndAndroid
    Sample app using RxJava and RxAndroid and Sample app using RxJava,
    RxAndroid and gradle-retrolambda for Java 8 Lambda support

---
+++ RxAndroidExample/app/src/main/java/com/jay/rxjava/FilterActivity.java
@@ -0,0 +1,36 @@
+package com.jay.rxjava;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+
+import com.jay.rxjava.observable.HelloWorldObservable;
+
+import rx.Observable;
+import rx.functions.Func1;
+
+
+public class FilterActivity extends Activity {
+
+  TextView mMessage;
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_filter);
+    mMessage = (TextView) findViewById(R.id.message);
+
+    Observable<String> observable = HelloWorldObservable.getObservableBackgroundThread();
+    observable.filter(new Func1<String, Boolean>() {
+      @Override
+      public Boolean call(String s) {
+        return !(s.contains(".") || s.contains("!"));
+      }
+    }).subscribe(mMessage::append, err->{}, ()->mMessage.append("\nDONE!"));
+
+  }
+
+
+
+}
